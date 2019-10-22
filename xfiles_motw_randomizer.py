274f2bd1ef8ad7a44c7e4608a6b79452c023f582
James Harris <jimmyharris@users.noreply.github.com>
2019-10-21 21:48:59 -0500

    initial commit
    Initial commit of xfiles data

---
+++ xfiles_motw_randomizer.py
@@ -0,0 +1,44 @@
+import requests
+from bs4 import BeautifulSoup
+import os
+
+from parse import parse
+
+import pandas as pd
+
+html = requests.get("https://x-files.fandom.com/wiki/Monster_of_the_Week")
+
+soup = BeautifulSoup(html.text)
+
+rows = soup.find("table", attrs={"class": "wikitable"}).find_all("tr")
+
+season = None
+
+motw_list = dict(season=[], episode=[], title=[], url=[])
+
+for row in rows:
+
+    headers = row.find_all("th", attrs={"colspan": "3"})
+
+    if headers:
+        season = parse("Season {season:d}", headers[0].text.strip())["season"]
+
+    cells = row.find_all("td")
+
+    if cells:
+        episode_num = int(cells[0].text)
+        title = cells[1].a.text
+        wikia_link = f'https://x-files.fandom.com{cells[1].a.get("href")}'
+        print(
+            's{season:02d}ep{ep:02d}: "{title}"'.format(
+                season=season, ep=episode_num, title=title
+            )
+        )
+        motw_list["season"].append(season)
+        motw_list["episode"].append(episode_num)
+        motw_list["title"].append(title)
+        motw_list["url"].append(wikia_link)
+
+motw_list = pd.DataFrame(motw_list)
+
+motw_list.to_csv(os.path.join(os.path.dirname(__file__), 'motw_files.csv'))
